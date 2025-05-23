7e25f8e064aa23915f6c0e27938962a772b7d87c
briannablacet <47799497+briannablacet@users.noreply.github.com>
2025-05-23 13:35:26 -0700

    Removed a link

--- src/pages/creation-hub.tsx
+++ src/pages/creation-hub.tsx
@@ -10,26 +10,8 @@ export default function CreationHubPage() {
   return (
     <NotificationProvider>
       <WritingStyleProvider>
-        {/* AI Insights Box */}
-        <div className="max-w-7xl mx-auto px-4 mt-6">
-          <div className="p-4 bg-blue-50 rounded-lg border border-blue-100 flex items-start">
-            <Lightbulb className="w-5 h-5 text-blue-600 mt-0.5 mr-3 flex-shrink-0" />
-            <div>
-              <h3 className="font-medium text-blue-800 mb-1">Pro Tip</h3>
-              <p className="text-blue-700">
-                Use a template from our template gallery to help you get started. 
-                <Link 
-                  href="/templates" 
-                  className="inline-flex items-center ml-2 text-blue-600 hover:text-blue-800 font-medium"
-                >
-                  Browse templates
-                  <ExternalLink className="w-3 h-3 ml-1" />
-                </Link>
-              </p>
-            </div>
-          </div>
-        </div>
-        
+
+
         <CreationHub />
       </WritingStyleProvider>
     </NotificationProvider>
