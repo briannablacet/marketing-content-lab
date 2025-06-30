15a59b8cd7a1286e976cc8c710af0515def07b79
briannablacet <47799497+briannablacet@users.noreply.github.com>
2025-06-30 15:20:09 -0700

    Removing extraneous WritingStyle call

--- src/pages/walkthrough/[step].tsx
+++ src/pages/walkthrough/[step].tsx
@@ -1,15 +1,12 @@
-// src/pages/walkthrough/[step].tsx
 import React from 'react';
-import { WritingStyleProvider } from '../../context/WritingStyleContext';
 import { NotificationProvider } from '../../context/NotificationContext';
 import MarketingWalkthrough from '../../components/features/MarketingWalkthrough';
 
 const WalkthroughStepPage: React.FC = () => {
   return (
     <NotificationProvider>
-      <WritingStyleProvider>
-        <MarketingWalkthrough />
-      </WritingStyleProvider>
+      {/* REMOVED: WritingStyleProvider - this should be in _app.tsx */}
+      <MarketingWalkthrough />
     </NotificationProvider>
   );
 };
